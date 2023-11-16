06f0d0494e7e8917b7a4cfea068eb7dd53f94ec5
DRain777 <d.cochew777@gmail.com>
2023-11-17 01:05:50 +0700

    task1

---
+++ index.js
@@ -0,0 +1,37 @@
+/* {
+  var a = 5;
+}
+
+console.log(a);
+ */
+
+/* let a = 5;
+a = 7;
+console.log(a); */
+
+/* let b = 1 + 2 + 3;
+console.log(b);
+
+ */
+
+// const c = 10;
+// const d = 2;
+// const sum = c + d ;
+// const sub = c - d; 
+// const mult = c * d; 
+// const div = c / d;
+// const result = sum + sub + mult;
+// console.log(result);
+
+
+
+// const name = 'Denis';
+// const a = 'Java';
+// const b = 'Script';
+// console.log(a + b)
+
+
+const ab = 'Hello';
+const cb = 'World';
+console.log(ab + ' ' + cb + '!');
+
