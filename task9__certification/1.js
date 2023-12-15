b69831bc2625682d716bb0ac71b9c9e989bcf868
ДЕНИС КОЧЕВ <106051662+DRain777@users.noreply.github.com>
2023-12-15 22:53:53 +0300

    Update 1.js

--- task9__certification/1.js
+++ task9__certification/1.js
@@ -6,35 +6,61 @@
 // быть отклонен с соответствующим сообщением об ошибке.
 // Работа должна быть выполнена с API: https://reqres.in/
 
-async function getUserData(userId) {
-  try {
-    const response = await fetch(`https://reqres.in/api/users/${userId}`);
 
-    if (response.ok) {
-      const userData = await response.json();
-      return userData.data; // Возвращаем объект данных о пользователе
-    } else {
-      const errorData = await response.json();
-      throw new Error(errorData.error || "Failed to fetch user data");
+  async function getUserData(id) {
+    const url = `https://reqres.in/api/users/${id}`;
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const user = await response.json();
+        return user.data;
+      } else {
+        throw Error(response.statusText);
+      }
+    } catch (error) {
+      console.log(error.message);
     }
-  } catch (error) {
-    console.error(error.message);
-    throw error; // Прокидываем ошибку дальше
   }
-}
 
-// Пример использования функции
-const userId = 2; // Замените на нужный ID пользователя
-getUserData(userId)
-  .then((userData) => {
-    console.log("User Data:", userData);
-  })
-  .catch((error) => {
-    console.error("Error:", error.message);
-  });
+  console.log(getUserData(3));
 
 
 
+
+
+
+
+
+
+
+
+
+// async function getUserData(userId) {
+//   try {
+//     const response = await fetch(`https://reqres.in/api/users/${userId}`);
+
+//     if (response.ok) {
+//       const userData = await response.json();
+//       return userData.data; // Возвращаем объект данных о пользователе
+//     } else {
+//       const errorData = await response.json();
+//       throw new Error(errorData.error || "Failed to fetch user data");
+//     }
+//   } catch (error) {
+//     console.error(error.message);
+//     throw error; // Прокидываем ошибку дальше
+//   }
+// }
+
+// // Пример использования функции
+// const userId = 2; // Замените на нужный ID пользователя
+// getUserData(userId)
+//   .then((userData) => {
+//     console.log("User Data:", userData);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error.message);
+//   });
 
 
 
