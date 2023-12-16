6603d6c57e652f5832c2e96330dc92a113e75785
ДЕНИС КОЧЕВ <106051662+DRain777@users.noreply.github.com>
2023-12-16 16:49:12 +0300

    Update 1.js

--- task9__certification/1.js
+++ task9__certification/1.js
@@ -6,68 +6,52 @@
 // быть отклонен с соответствующим сообщением об ошибке.
 // Работа должна быть выполнена с API: https://reqres.in/
 
-
-  async function getUserData(id) {
-    const url = `https://reqres.in/api/users/${id}`;
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const user = await response.json();
-        return user.data;
-      } else {
-        throw Error(response.statusText);
-      }
-    } catch (error) {
-      console.log(error.message);
+async function getUserData(id) {
+  const url = `https://reqres.in/api/users/${id}`;
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const user = await response.json();
+      return user.data;
+    } else {
+      throw Error(response.statusText);
     }
+  } catch (error) {
+    console.log(error.message);
   }
+}
 
-  console.log(getUserData(3));
+console.log(getUserData(3));
+getUserData(3).then((userData) => console.log(userData));
 
+// async function: Это асинхронная функция, что означает, что она может использовать оператор await для ожидания асинхронных операций.
 
+// const url = https://reqres.in/api/users/${id}`;`: Создается URL для запроса на API reqres.in с указанным id.
 
+// const response = await fetch(url);: Используется fetch для отправки HTTP-запроса по указанному URL. Оператор await позволяет дождаться завершения этой асинхронной операции.
 
+// if (response.ok) { ... } else { ... }: Проверяется, успешен ли HTTP-запрос. Если ответ сервера имеет статус "OK" (в пределах 200-299), код продолжает выполнение. В противном случае генерируется ошибка.
 
+// const user = await response.json();: Если запрос успешен, извлекается тело ответа в формате JSON. Оператор await снова ожидает завершение асинхронной операции.
 
+// return user.data;: Возвращается часть данных пользователя из JSON-ответа.
 
+// throw Error(response.statusText);: Если HTTP-запрос не успешен, генерируется ошибка с текстом статуса ответа.
 
+// catch (error) { ... }: Обрабатывается ошибка, если она произошла в блоке try.
 
+// console.log(error.message);: Выводится сообщение об ошибке в консоль.
 
+// throw error;: Ошибка снова выбрасывается (пропагируется дальше), чтобы ее можно было обработать в вызывающем коде, если это необходимо.
 
+// Вызов функции и вывод результата в консоль:
 
-// async function getUserData(userId) {
-//   try {
-//     const response = await fetch(`https://reqres.in/api/users/${userId}`);
+// console.log(getUserData(3));
+// Функция getUserData(3) вызывается с аргументом 3 (это id пользователя). Однако, так как getUserData является асинхронной функцией, она возвращает Promise. Прямой вывод console.log(getUserData(3)); не даст ожидаемого результата.
 
-//     if (response.ok) {
-//       const userData = await response.json();
-//       return userData.data; // Возвращаем объект данных о пользователе
-//     } else {
-//       const errorData = await response.json();
-//       throw new Error(errorData.error || "Failed to fetch user data");
-//     }
-//   } catch (error) {
-//     console.error(error.message);
-//     throw error; // Прокидываем ошибку дальше
-//   }
-// }
+// Для правильного использования результат функции можно обработать с использованием then:
 
-// // Пример использования функции
-// const userId = 2; // Замените на нужный ID пользователя
-// getUserData(userId)
-//   .then((userData) => {
-//     console.log("User Data:", userData);
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error.message);
-//   });
-
-
-
-
-
-
-
-
+//  getUserData(3).then(userData => console.log(userData));
+// Или использовать async/await в контексте другой асинхронной функции:
 
 
